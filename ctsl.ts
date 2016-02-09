c46502233fc24c3327b803a87050b0902a84143d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2016-02-09 23:19:19 +0100

    Emit JS code as well as Ceylon code
    Eventually we’ll wrap some model loading boilerplate around the JS code
    as well of course.

--- ctsl.ts
+++ ctsl.ts
@@ -9,93 +9,106 @@ const program : ts.Program = ts.createProgram(["simple.ts"], {});
 const sourceFiles : ts.SourceFile[] = program.getSourceFiles();
 const sourceFile : ts.SourceFile = sourceFiles[sourceFiles.length - 1];
 const checker : ts.TypeChecker = program.getTypeChecker();
-const fd : number = fs.openSync("simple.ceylon", "w");
+const fd_ceylon : number = fs.openSync("simple.ceylon", "w");
+const fd_js : number = fs.openSync("simple.js", "w");
 
-function write(text: string = ""): void {
+function write(fd: number, text: string): void {
     (<any>fs).writeSync(fd, text); // the DefinitelyTyped file is missing most of the writeSync overloads, so remove type info
 }
 
-function writeLine(line: string = ""): void {
-    write(line + "\n");
+function writeCeylon(text: string = ""): void {
+    write(fd_ceylon, text);
+}
+
+function writeCeylonLine(line: string = ""): void {
+    writeCeylon(line + "\n");
+}
+
+function writeJs(text: string = ""): void {
+    write(fd_js, text);
+}
+
+function writeJsLine(line: string = ""): void {
+    writeJs(line + "\n");
 }
 
 // TODO force case
 function emitName(name: ts.EntityName | ts.DeclarationName): void {
-    write(" ");
+    writeCeylon(" ");
     switch (name.kind) {
     case (ts.SyntaxKind.Identifier): {
-        write((<ts.Identifier>name).text);
+        writeCeylon((<ts.Identifier>name).text);
         break;
     }
     case (ts.SyntaxKind.StringLiteral): {
-        write((<ts.StringLiteral>name).text);
+        writeCeylon((<ts.StringLiteral>name).text);
         break;
     }
     case (ts.SyntaxKind.QualifiedName): {
         const qn = <ts.QualifiedName>name;
         emitName(qn.left);
-        write(".");
+        writeCeylon(".");
         emitName(qn.right);
         break;
     }
     default: {
-        write("/* TODO: unknown name kind " + name.kind + " */ Anything");
+        writeCeylon("/* TODO: unknown name kind " + name.kind + " */ Anything");
         break;
     }
     }
-    write(" ");
+    writeCeylon(" ");
 }
 
 function emitType(type: ts.TypeNode): void {
-    write(" ");
+    writeCeylon(" ");
     switch (type.kind) {
     case (ts.SyntaxKind.AnyKeyword): {
-        write("dynamic");
+        writeCeylon("dynamic");
         break;
     }
     case (ts.SyntaxKind.StringKeyword): {
-        write("String");
+        writeCeylon("String");
         break;
     }
     case (ts.SyntaxKind.NumberKeyword): {
-        write("Integer|Float");
+        writeCeylon("Integer|Float");
         break;
     }
     case (ts.SyntaxKind.BooleanKeyword): {
-        write("Boolean");
+        writeCeylon("Boolean");
         break;
     }
     case (ts.SyntaxKind.VoidKeyword): {
-        write("void");
+        writeCeylon("void");
         break;
     }
     case (ts.SyntaxKind.TypeReference): {
         const tr = <ts.TypeReferenceNode>type;
         if ((<ts.Identifier>tr.typeName).text === "Function") {
             // special case: global Function type for any function
-            write("Nothing(Anything*)");
+            writeCeylon("Nothing(Anything*)");
             break;
         }
         emitName(tr.typeName);
         const tas = tr.typeArguments;
         if (tas) {
-            write("<");
+            writeCeylon("<");
             let needComma: boolean = false;
             for (const ta of tas) {
-                if (needComma) write(",");
+                if (needComma) writeCeylon(",");
                 emitType(ta);
                 needComma = true;
             }
-            write(">");
+            writeCeylon(">");
         }
         break;
     }
     default: {
-        write("/* TODO: unknown type kind " + type.kind + " */ Anything");
+        writeCeylon("/* TODO: unknown type kind " + type.kind + " */ Anything");
         break;
     }
     }
-    write(" ");
+    writeCeylon(" ");
 }
 
 function emitSignature(decl: ts.SignatureDeclaration): void {
@@ -104,72 +117,72 @@ function emitSignature(decl: ts.SignatureDeclaration): void {
     if (decl.name)
         emitName(decl.name);
     // TODO type parameters
-    write("(");
+    writeCeylon("(");
     let needComma: boolean = false
     for (const parameter of decl.parameters) {
-        if (needComma) write(", ");
+        if (needComma) writeCeylon(", ");
         emitType(parameter.type);
         emitName(parameter.name);
         // TODO other stuff
         needComma = true;
     }
-    write(")");
+    writeCeylon(")");
 }
 
 function emitDeclaration(decl: ts.Declaration): void {
     switch (decl.kind) {
     case (ts.SyntaxKind.InterfaceDeclaration): {
         const idecl = <ts.InterfaceDeclaration>decl;
-        write("shared dynamic");
+        writeCeylon("shared dynamic");
         emitName(idecl.name);
-        writeLine("{");
+        writeCeylonLine("{");
         for (const memberName in idecl.members) {
             const member = idecl.members[memberName];
             if (typeof member === "number") continue; // pos, end; TODO what happens if members are actually called that?
             emitDeclaration(member);
         }
-        writeLine("}");
+        writeCeylonLine("}");
         break;
     }
     case (ts.SyntaxKind.ClassDeclaration): {
         const cdecl = <ts.ClassDeclaration>decl;
-        write("shared class");
+        writeCeylon("shared class");
         emitName(cdecl.name);
-        writeLine("{");
+        writeCeylonLine("{");
         for (const memberName in cdecl.members) {
             const member = cdecl.members[memberName];
             if (typeof member === "number") continue; // pos, end; TODO what happens if members are actually called that?
             emitDeclaration(member);
         }
-        writeLine("}");
+        writeCeylonLine("}");
         break;
     }
     case (ts.SyntaxKind.PropertySignature):
     case (ts.SyntaxKind.PropertyDeclaration): {
         const pdecl = <ts.PropertySignature|ts.PropertyDeclaration>decl;
-        write("shared formal ");
+        writeCeylon("shared formal ");
         emitType(pdecl.type);
         emitName(pdecl.name);
-        writeLine(";");
+        writeCeylonLine(";");
         break;
     }
     case (ts.SyntaxKind.MethodSignature):
     case (ts.SyntaxKind.MethodDeclaration): {
         const mdecl = <ts.MethodSignature|ts.MethodDeclaration>decl;
-        write("shared formal ");
+        writeCeylon("shared formal ");
         emitSignature(mdecl);
-        writeLine(";");
+        writeCeylonLine(";");
         break;
     }
     case (ts.SyntaxKind.Constructor): {
         const cdecl = <ts.ConstructorDeclaration>decl;
-        write("shared new ");
+        writeCeylon("shared new ");
         emitSignature(cdecl);
-        writeLine("{}");
+        writeCeylonLine("{}");
         break;
     }
     default: {
-        writeLine("/* TODO: unknown declaration kind " + decl.kind + " */");
+        writeCeylonLine("/* TODO: unknown declaration kind " + decl.kind + " */");
     }
     }
 }
@@ -178,3 +191,7 @@ for (const declName in sourceFile.locals) {
     const decl = sourceFile.locals[declName];
     emitDeclaration(decl.declarations[0]);
 }
+
+program.emit(sourceFile, function(fileName: string, data: string, writeByteOrderMark: boolean, onError: (message: string) => void): void {
+    writeJs(data);
+});
