5146d998a275dbbd46b060387d92a366485ebcb7
Lucas Werkmeister <mail@lucaswerkmeister.de>
2016-02-20 19:15:04 +0100

    Start emitting model files directly.
    Lots of changes in here.
    
    - We now emit the model file directly, instead of writing Ceylon code
      and compiling that with the Ceylon compiler.
    - This makes it unnecessary to have a Ceylon source layout for the
      TypeScript module, and therefore, simple.ts moves to toplevel, and the
      module.ceylon and package.ceylon for the simple module are gone.
    - Most of ctsl.ts is ripped out, since the emitting Ceylon is no longer
      useful. A tiny bit, rewritten to emit a model instead, remains; the
      rest, and more, will now be added back, bit by bit.
    - The Makefile is almost completely rewritten, and now does a lot more
      work. A simple `make` will build tsc, ctsl, run ctsl, compute
      SHA1sums, and attempt to compile and run a second Ceylon module
      (simple2) that depends on simple. If everything works, that will exit
      with 0; otherwise, there’ll be an error somewhere along the way. (Of
      course, ctsl should eventually write SHA1sums itself, but doing it in
      the Makefile should be enough until the rewrite in Ceylon.)

--- Makefile
+++ Makefile
@@ -1,23 +1,38 @@
 # Jake apparently ignores -C when looking for the Jakefile, so it needs -f
-TSJAKE  := jake -C TypeScript -f TypeScript/Jakefile.js
-NODE    := node
+TSJAKE := jake -C TypeScript -f TypeScript/Jakefile.js
+NODE   := node
 
-TSFILES := $(shell jq -r .files[] tsconfig.json)
-JSFILES := $(patsubst %.ts,built/%.js,$(TSFILES))
+CTSL_TSFILE := ctsl.ts
+CTSL_JSFILE := built/ctsl.js
 
-.PHONY: all run clean
+MODULE_NAME       := simple
+MODULE_VERSION    := 1.0.0
+MODULE_TSFILE     := $(MODULE_NAME).ts
+MODULE_JSFILE     := modules/$(MODULE_NAME)/$(MODULE_VERSION)/$(MODULE_NAME)-$(MODULE_VERSION).js
+MODULE_MODELFILE  := modules/$(MODULE_NAME)/$(MODULE_VERSION)/$(MODULE_NAME)-$(MODULE_VERSION)-model.js
 
-all: $(JSFILES)
+TESTMODULE_NAME    := simple2
+TESTMODULE_VERSION := 1.0.0
 
-run: TypeScript/built/local/typescript.js $(JSFILES)
-	cat $^ | $(NODE)
+.PHONY: all test clean
 
-$(JSFILES): TypeScript/built/local/tsc.js $(TSFILES)
-	$(NODE) $<
+all: test
 
 TypeScript/built/local/typescript.js TypeScript/built/local/tsc.js:
 	$(TSJAKE) local
 
+$(CTSL_JSFILE): TypeScript/built/local/tsc.js $(CTSL_TSFILE)
+	$(NODE) $<
+
+$(MODULE_JSFILE) $(MODULE_MODELFILE): TypeScript/built/local/typescript.js $(CTSL_JSFILE) $(MODULE_TSFILE)
+	cat TypeScript/built/local/typescript.js $(CTSL_JSFILE) | $(NODE)
+
+%.sha1: %
+	printf "%s" "$$(sha1sum $^ | cut -d' ' -f1)" > $@
+
+test: $(MODULE_JSFILE) $(MODULE_JSFILE).sha1 $(MODULE_MODELFILE) $(MODULE_MODELFILE).sha1
+	ceylon compile-js,run-js $(TESTMODULE_NAME)
+
 clean:
-	$(RM) $(JSFILES)
+	$(RM) $(CTSL_JSFILE) $(MODULE_JSFILE) $(MODULE_JSFILE).sha1 $(MODULE_MODELFILE) $(MODULE_MODELFILE).sha1
 	$(TSJAKE) clean
--- ctsl.ts
+++ ctsl.ts
@@ -7,23 +7,15 @@ import * as fs from "fs";
 
 const modname : string = "simple";
 const modver : string = "1.0.0";
-const program : ts.Program = ts.createProgram([`source/${modname}/${modname}.ts`], {});
+const program : ts.Program = ts.createProgram([`${modname}.ts`], {});
 const sourceFiles : ts.SourceFile[] = program.getSourceFiles();
 const sourceFile : ts.SourceFile = sourceFiles[sourceFiles.length - 1];
 const checker : ts.TypeChecker = program.getTypeChecker();
-const fd_ceylon : number = fs.openSync(`source/${modname}/${modname}.ceylon`, "w");
 const fd_js : number = fs.openSync(`modules/${modname}/${modver}/${modname}-${modver}.js`, "w");
+const fd_model : number = fs.openSync(`modules/${modname}/${modver}/${modname}-${modver}-model.js`, "w");
 
 function write(fd: number, text: string): void {
     (<any>fs).writeSync(fd, text); // the DefinitelyTyped file is missing most of the writeSync overloads, so remove type info
-}
-
-function writeCeylon(text: string = ""): void {
-    write(fd_ceylon, text);
-}
-
-function writeCeylonLine(line: string = ""): void {
-    writeCeylon(line + "\n");
 }
 
 function writeJs(text: string = ""): void {
@@ -34,172 +26,59 @@ function writeJsLine(line: string = ""): void {
     writeJs(line + "\n");
 }
 
-// TODO force case
-function emitName(name: ts.EntityName | ts.DeclarationName): void {
-    writeCeylon(" ");
-    switch (name.kind) {
-    case (ts.SyntaxKind.Identifier): {
-        writeCeylon((<ts.Identifier>name).text);
-        break;
-    }
-    case (ts.SyntaxKind.StringLiteral): {
-        writeCeylon((<ts.StringLiteral>name).text);
-        break;
-    }
-    case (ts.SyntaxKind.QualifiedName): {
-        const qn = <ts.QualifiedName>name;
-        emitName(qn.left);
-        writeCeylon(".");
-        emitName(qn.right);
-        break;
-    }
-    default: {
-        writeCeylon("/* TODO: unknown name kind " + name.kind + " */ Anything");
-        break;
-    }
-    }
-    writeCeylon(" ");
+function writeModel(text: string = ""): void {
+    write(fd_model, text);
+}
+
+function error(line: string): void {
+    write(2, `ctsl: ${line}\n`);
 }
 
 function emitType(type: ts.TypeNode): void {
-    writeCeylon(" ");
     switch (type.kind) {
-    case (ts.SyntaxKind.AnyKeyword): {
-        writeCeylon("dynamic");
-        break;
-    }
     case (ts.SyntaxKind.StringKeyword): {
-        writeCeylon("String");
-        break;
-    }
-    case (ts.SyntaxKind.NumberKeyword): {
-        writeCeylon("Integer|Float");
-        break;
-    }
-    case (ts.SyntaxKind.BooleanKeyword): {
-        writeCeylon("Boolean");
-        break;
-    }
-    case (ts.SyntaxKind.VoidKeyword): {
-        writeCeylon("void");
-        break;
-    }
-    case (ts.SyntaxKind.TypeReference): {
-        const tr = <ts.TypeReferenceNode>type;
-        if ((<ts.Identifier>tr.typeName).text === "Function") {
-            // special case: global Function type for any function
-            writeCeylon("Nothing(Anything*)");
-            break;
-        }
-        emitName(tr.typeName);
-        const tas = tr.typeArguments;
-        if (tas) {
-            writeCeylon("<");
-            let needComma: boolean = false;
-            for (const ta of tas) {
-                if (needComma) writeCeylon(",");
-                emitType(ta);
-                needComma = true;
-            }
-            writeCeylon(">");
-        }
+        writeModel('{md:"$",pk:"$",nm:"String"}');
         break;
     }
     default: {
-        writeCeylon("/* TODO: unknown type kind " + type.kind + " */ Anything");
+        error("unknown type kind " + type.kind);
         break;
     }
     }
-    writeCeylon(" ");
-}
-
-function emitSignature(decl: ts.SignatureDeclaration): void {
-    if (decl.type)
-        emitType(decl.type);
-    if (decl.name)
-        emitName(decl.name);
-    // TODO type parameters
-    writeCeylon("(");
-    let needComma: boolean = false
-    for (const parameter of decl.parameters) {
-        if (needComma) writeCeylon(", ");
-        emitType(parameter.type);
-        emitName(parameter.name);
-        // TODO other stuff
-        needComma = true;
-    }
-    writeCeylon(")");
 }
 
 function emitDeclaration(decl: ts.Declaration): void {
     switch (decl.kind) {
-    case (ts.SyntaxKind.InterfaceDeclaration): {
-        const idecl = <ts.InterfaceDeclaration>decl;
-        writeCeylon("shared dynamic");
-        emitName(idecl.name);
-        writeCeylonLine("{");
-        for (const memberName in idecl.members) {
-            const member = idecl.members[memberName];
-            if (typeof member === "number") continue; // pos, end; TODO what happens if members are actually called that?
-            emitDeclaration(member);
-        }
-        writeCeylonLine("}");
-        break;
-    }
-    case (ts.SyntaxKind.ClassDeclaration): {
-        const cdecl = <ts.ClassDeclaration>decl;
-        writeCeylon("shared class");
-        emitName(cdecl.name);
-        writeCeylonLine("{");
-        for (const memberName in cdecl.members) {
-            const member = cdecl.members[memberName];
-            if (typeof member === "number") continue; // pos, end; TODO what happens if members are actually called that?
-            emitDeclaration(member);
-        }
-        writeCeylonLine("}");
-        break;
-    }
-    case (ts.SyntaxKind.PropertySignature):
-    case (ts.SyntaxKind.PropertyDeclaration): {
-        const pdecl = <ts.PropertySignature|ts.PropertyDeclaration>decl;
-        writeCeylon("shared formal ");
-        emitType(pdecl.type);
-        emitName(pdecl.name);
-        writeCeylonLine(";");
-        break;
-    }
-    case (ts.SyntaxKind.MethodSignature):
-    case (ts.SyntaxKind.MethodDeclaration): {
-        const mdecl = <ts.MethodSignature|ts.MethodDeclaration>decl;
-        writeCeylon("shared formal ");
-        emitSignature(mdecl);
-        writeCeylonLine(";");
-        break;
-    }
     case (ts.SyntaxKind.FunctionDeclaration): {
         const fdecl = <ts.FunctionDeclaration>decl;
-        writeCeylon("shared ");
-        emitSignature(fdecl);
-        writeCeylonLine("=> nothing;");
-        break;
-    }
-    case (ts.SyntaxKind.Constructor): {
-        const cdecl = <ts.ConstructorDeclaration>decl;
-        writeCeylon("shared new ");
-        emitSignature(cdecl);
-        writeCeylonLine("{}");
+        const name = fdecl.name.text;
+        writeModel(`,${name}:{$t:`);
+        emitType(fdecl.type);
+        writeModel(`,pa:1,mt:"m",nm:"${name}"}`);
         break;
     }
     default: {
-        writeCeylonLine("/* TODO: unknown declaration kind " + decl.kind + " */");
+        error("unknown declaration kind " + decl.kind);
+        break;
     }
     }
 }
+
+writeModel(`(function(define) { define(function(require, ex$, module) {
+ex$.$CCMM$={"$mod-version":"${modver}","$mod-deps":["ceylon.language\/1.2.1"],"$mod-bin":"9.0",${modname}:{"$pkg-pa":1`);
 
 for (const declName in sourceFile.locals) {
     const decl = sourceFile.locals[declName];
     emitDeclaration(decl.declarations[0]);
 }
+
+writeModel(`},"$mod-name":"${modname}"};
+});
+}(typeof define==='function' && define.amd ? define : function (factory) {
+if (typeof exports!=='undefined') { factory(require, exports, module);
+} else { throw 'no module loader'; }
+}));
+`);
 
 writeJs(`(function(define) { define(function(require, ex$, module) {
 
