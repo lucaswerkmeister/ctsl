af56bdf53f2d7814dea1aa95af8b576849bc406c
Lucas Werkmeister <mail@lucaswerkmeister.de>
2016-03-21 23:02:22 +0100

    Upgrade to Ceylon 1.2.3

--- ctsl.ts
+++ ctsl.ts
@@ -5,7 +5,7 @@
 
 import * as fs from "fs";
 
-const langver : string = "1.2.2";
+const langver : string = "1.2.3";
 const modname : string = "simple";
 const modver : string = "1.0.0";
 const program : ts.Program = ts.createProgram([`${modname}.ts`], {});
--- source/simple2/module.ceylon
+++ source/simple2/module.ceylon
@@ -1,4 +1,4 @@
 module simple2 "1.0.0" {
     import simple "1.0.0";
-    import ceylon.test "1.2.2";
+    import ceylon.test "1.2.3";
 }
